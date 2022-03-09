f0f1b6b905a99a52be89bd5d17f077895b41240f
damienleee <damienleee99@gmail.com>
2022-03-09 20:37:48 +0800

    add ACSF main script

--- ACSF_featurizer.py
+++ ACSF_featurizer.py
@@ -1,5 +1,5 @@
 from crystal_out import crystalOut
-from dscribe.descriptors import SOAP
+from dscribe.descriptors import ACSF
 from pymatgen.io.ase import AseAtomsAdaptor
 
 Na3PO4_114 = crystalOut("crystal17_output_files/calc-Na3PO4_114_icsd97205_tzvp_PBE0/Na3PO4_114.out")
@@ -8,12 +8,11 @@ Na3PS4_114 = crystalOut("crystal17_output_files/calc-Na3PS4_114_icsd121566_tzvp_
 O_ASE = AseAtomsAdaptor.get_atoms(Na3PO4_114.structure)
 S_ASE = AseAtomsAdaptor.get_atoms(Na3PS4_114.structure)
 
-soap = SOAP(periodic=True, 
-    rcut=6,
-    nmax=8,
-    lmax=6,
-    sigma=1,
-    species = ["Na", "P", "S", "O", "Li"],
-    average="inner")
-SOAP_features = soap.create([S_ASE, O_ASE]) 
-print(SOAP_features, SOAP_features.shape)
+acsf = ACSF(species = ["Na", "P", "S", "O", "Li"],
+    rcut=6.0,
+    periodic = True,
+    g2_params=[[1, 1], [1, 2], [1, 3]],
+    g4_params=[[1, 1, 1], [1, 2, 1], [1, 1, -1], [1, 2, -1]],
+)
+ACSF_features = acsf.create([S_ASE, O_ASE])
+print(ACSF_features, ACSF_features[0].shape, ACSF_features[1].shape)
