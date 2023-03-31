a89f2ded75a39e14c3a64b1975ba84bb5cbd9a8e
Yuheng Li <yul549@eng.ucsd.edu>
2023-03-31 11:24:12 +0800

    web interface

---
+++ web_interface/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+#import numpy as np
+#import pandas as pd
+#import plotly.express as px
+
+st.markdown("# Main page")
+st.sidebar.markdown("# Main page")
+
+st.title("Hybrid-Functional Computational Raman Database for Inorganic Compounds")
---
+++ web_interface/pages/compound_list.py
@@ -0,0 +1,7 @@
+import streamlit as st
+#import numpy as np
+#import pandas as pd
+#import plotly.express as px
+
+st.markdown("# Compound List")
+st.sidebar.markdown("# Compound List")
